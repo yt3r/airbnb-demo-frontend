7a1a52df1f7ceaca14d50dafff2e134eba0bea15
bunnycat <bunnycat@ohai.pc>
2017-11-09 19:50:12 +0400

    Modified Homes component

--- src/Homes/Homes.js
+++ src/Homes/Homes.js
@@ -13,8 +13,7 @@ import {
   Description,
   RatingInfo,
   SectionHead,
-  Scroll,
-  ScrollArrow
+  Scroll
 } from "../styled";
 import home01 from "./home-1.png";
 import home02 from "./home-2.png";
@@ -27,9 +26,26 @@ const Homes = SectionContainerBase.extend`
 
 const Card = CardBase.extend`
   flex-basis: 33.3333%;
-  padding-right: 8px;
   padding-left: 8px;
 `;
+
+const HomesCard = props => (
+  <Card>
+    <Img src={props.src} alt={props.name} />
+    <Info>
+      {props.price} {props.name}
+    </Info>
+    <Description>
+      {props.type} &middot; {props.beds}
+    </Description>
+    <Rating>
+      <Stars />
+      <RatingInfo>
+        {props.rating} &middot; {props.status}
+      </RatingInfo>
+    </Rating>
+  </Card>
+);
 
 const Img = styled.img`width: 100%;`;
 
@@ -43,33 +59,33 @@ export default () => {
         </Link>
       </SectionHead>
       <Cards>
-        <Card>
-          <Img src={home01} alt="home img" />
-          <Info>$89 La Salentina, see, nature & relax</Info>
-          <Description>Entire house &middot; 9 beds</Description>
-          <Rating>
-            <Stars />
-            <RatingInfo>97 &middot; Superhost</RatingInfo>
-          </Rating>
-        </Card>
-        <Card>
-          <Img src={home02} alt="home img" />
-          <Info>$89 Your private 3 bedr.</Info>
-          <Description>Entire house &middot; 5 beds</Description>
-          <Rating>
-            <Stars />
-            <RatingInfo>161 &middot; Superhost</RatingInfo>
-          </Rating>
-        </Card>
-        <Card>
-          <Img src={home03} alt="home img" />
-          <Info>$200 Dreamy Tropical Tree House</Info>
-          <Description>Entire treehouse &middot; 1 bed</Description>
-          <Rating>
-            <Stars />
-            <RatingInfo>364 &middot; Superhost</RatingInfo>
-          </Rating>
-        </Card>
+        <HomesCard
+          src={home01}
+          price="$89"
+          name="La Salentina, see, nature & relax"
+          type="Entire house"
+          beds="9 beds"
+          rating="97"
+          status="Superhost"
+        />
+        <HomesCard
+          src={home02}
+          price="$89"
+          name="Your private 3 bedr."
+          type="Entire house"
+          beds="5 beds"
+          rating="161"
+          status="Superhost"
+        />
+        <HomesCard
+          src={home03}
+          price="$200"
+          name="Dreamy Tropical Tree House"
+          type="Entire treehouse"
+          beds="1 bed"
+          rating="364"
+          status="Superhost"
+        />
         <Scroll />
       </Cards>
     </Homes>
